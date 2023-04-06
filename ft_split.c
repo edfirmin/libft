1817516aa9fd11bce824fb4f8e21ae624d1f4158
Edilson Firmino <edfirmin@c2r2p4.42nice.fr>
2023-04-06 15:08:55 +0200

    mapi et iteri

--- ft_split.c
+++ ft_split.c
@@ -6,59 +6,92 @@
 /*   By: edfirmin <edfirmin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 09:07:42 by edfirmin          #+#    #+#             */
-/*   Updated: 2023/04/06 10:23:15 by edfirmin         ###   ########.fr       */
+/*   Updated: 2023/04/06 12:40:45 by edfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*strcpy(char *dest, char const *src, char ch)
+static size_t	nb_word(char const *str, char ch)
+{
+	size_t	j;
+	int		i;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		while (str[i] == ch)
+			i++;
+		if (str[i] != ch && (str[i + 1] == ch || str[i + 1] == 0))
+			j++;
+		i++;
+	}
+	return (j);
+}
+
+static int	size_word(char const *str, char ch)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[i] == ch)
+		i++;
+	while (str[i] != ch)
+		j++;
+	return (j);
+}
+
+static char	*copy(char *dest, const char *src, char ch)
 {
 	int	i;
 
 	i = 0;
-	while (src[i] != ch)
+	while (src[i] && src[i] != ch)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**tab_str;
-	int		i;
-	int		j;
-	int		n;
+	char		**tab_str;
+	int			i;
+	size_t		j;
+	size_t		k;
 
 	i = 0;
 	j = 0;
-	tab_str = 0;
-	while (s[i])
+	k = nb_word(s, c);
+	tab_str = malloc(k + 1 * sizeof(char *));
+	if (!tab_str)
 	{
-		n = 0;
-		s = &s[i];
-		while (s[i] != c)
-		{
+		free(tab_str);
+		return (0);
+	}
+	while (s[i] && j < k)
+	{
+		while (s[i] == c)
 			i++;
-			n++;
+		tab_str[j] = malloc(size_word(&s[i], c) * sizeof(char));
+		if (!tab_str[j])
+		{
+			while (j >= 0)
+			{
+				free(tab_str[j]);
+				j--;
+			}
+			free(tab_str);
+			return (0);
 		}
-		tab_str[j] = malloc((n + 1) * sizeof(char));
-		strcpy(tab_str[j], s, c);
+		copy(tab_str[j], &s[i], c);
+		i += size_word(&s[i], c);
 		j++;
 		i++;
 	}
 	return (tab_str);
 }
-/*
-#include <stdio.h>
-
-int	main(void)
-{
-	char	**oui;
-
-	oui = ft_split("je suis une mangue :)", 'u');
-	printf("%s\n", oui[0]);
-}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: edfirmin <edfirmin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 11:52:33 by edfirmin          #+#    #+#             */
-/*   Updated: 2023/04/06 10:21:31 by edfirmin         ###   ########.fr       */
+/*   Updated: 2023/04/06 10:46:43 by edfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_itoa(int n);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif
